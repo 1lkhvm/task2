cccaa5eb74e350e2333d10631ebfd88ea133f091
1lkhvm <tu6wkkvyr@code.edu.az>
2022-10-31 23:28:09 +0400

    :(

---
+++ task1/Program.cs
@@ -0,0 +1,36 @@
+﻿//{
+//   // int userNumber = 5;
+//   // int factoryal = 1;
+//   // for (int i = 1; i <= userNumber; i++)
+//   // {
+//    //    factoryal *= i; //factoryal = factoryal *i
+//    //}
+//    //Console.WriteLine(factoryal);
+//    //task2
+//}
+
+
+//int UNumber = 5;
+//int count = 0;
+
+//for (int i = 1; i <= UNumber ; i++)
+//{
+//    if (UNumber%i == 0)
+//    {
+//        count++;
+//    }
+//}
+//if (UNumber == 1)
+//{
+//    Console.WriteLine("DAxil etdiyiniz eded ne sade ne de murekkebdir!!");
+//}
+//else if (count > 2)
+//{
+//    Console.WriteLine("Daxil etdyiniz eded Murekkeb ededdir");
+//}
+//else 
+//{
+//    Console.WriteLine("Daxil etdiyiniz eded Sade ededdir");
+//}    
+
+
---
+++ tsk1.js
@@ -0,0 +1,33 @@
+// {
+//     var userNumber = 5;
+//     var factoryal = 1;
+//     for (var i = 1; i <= userNumber; i++)
+//     {
+//         factoryal *= i; //factoryal = factoryal *i
+//     }
+//     console.log(factoryal); 
+// }
+
+
+// var UNumber = 2;
+// var count = 0;
+
+// for (var i = 1; i <= UNumber ; i++)
+// {
+//     if (UNumber%i == 0)
+//     {
+//         count++;
+//     }
+// }
+// if (UNumber == 1)
+// {
+//     Console.log("DAxil etdiyiniz eded ne sade ne de murekkebdir!!");
+// }
+// else if (count > 2)
+// {
+//     Console.log();("Daxil etdyiniz eded Murekkeb ededdir");
+// }
+// else 
+// {
+// console.log(Daxil etdiyiniz eded Sade ededdir);
+// }    
